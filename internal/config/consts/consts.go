41afa2427289b8acf10d362236c5e819f3824a11
Афанасьев Сергей Игоревич <sergey.afanasev@fleetcor.ru>
2024-06-10 23:42:38 +0300

    Request logger add body

--- internal/config/consts/consts.go
+++ internal/config/consts/consts.go
@@ -8,3 +8,8 @@ const (
 	HttpRateRequestLimit = 100
 	HttpRateWindowLength = 1 * time.Minute
 )
+
+var (
+	SensitiveDataMask  = "*****"
+	SensitiveFilerKeys = []string{"password"}
+)
--- internal/infrastructure/http/middleware/request_logger.go
+++ internal/infrastructure/http/middleware/request_logger.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"time"
 
 	chimiddleware "github.com/go-chi/chi/v5/middleware"
 
+	"github.com/seregaa020292/ModularMonolith/internal/config/consts"
 	"github.com/seregaa020292/ModularMonolith/pkg/utils/gog"
 )
 
@@ -17,8 +21,9 @@ type (
 		logger *slog.Logger
 	}
 	entryLogger struct {
-		req    *http.Request
 		logger *slog.Logger
+		req    *http.Request
+		body   map[string]any
 	}
 )
 
@@ -32,7 +37,9 @@ func GetEntryLogger(ctx context.Context) *slog.Logger {
 func SetEntryLoggerCtxFromWriter(w http.ResponseWriter) context.Context {
 	return context.WithValue(context.Background(),
 		chimiddleware.LogEntryCtxKey,
-		slog.With(slog.String("correlation_id", GetCorrelationIDResponse(w))),
+		&entryLogger{
+			logger: slog.With(slog.String("correlation_id", GetCorrelationIDResponse(w))),
+		},
 	)
 }
 
@@ -44,11 +51,12 @@ func NewRequestLogger(logger *slog.Logger) *RequestLogger {
 
 func (l RequestLogger) NewLogEntry(r *http.Request) chimiddleware.LogEntry {
 	return &entryLogger{
-		req: r,
 		logger: l.logger.With(
 			slog.String("request_id", chimiddleware.GetReqID(r.Context())),
 			slog.String("correlation_id", GetCorrelationID(r.Context())),
 		),
+		req:  r,
+		body: sanitizeRequestBody(r, consts.SensitiveDataMask, consts.SensitiveFilerKeys),
 	}
 }
 
@@ -57,10 +65,11 @@ func (l entryLogger) Write(status, bytes int, header http.Header, elapsed time.D
 		slog.String("url", fmt.Sprintf("%s://%s%s %s",
 			gog.If(l.req.TLS != nil, "https", "http"), l.req.Host, l.req.RequestURI, l.req.Proto)),
 		slog.String("method", l.req.Method),
-		slog.String("user_agent", l.req.UserAgent()),
 		slog.Int("status", status),
-		slog.Int("bytes", bytes),
+		slog.Any("body", l.body),
+		slog.String("user_agent", l.req.UserAgent()),
 		slog.Float64("elapsed", float64(elapsed.Nanoseconds())/1000000.0),
+		slog.Int("bytes", bytes),
 	)
 
 	switch {
@@ -78,4 +87,29 @@ func (l entryLogger) Panic(v any, stack []byte) {
 		slog.String("stack", string(stack)),
 		slog.String("panic", fmt.Sprintf("%+v", v)),
 	)
+}
+
+// sanitizeRequestBody читает и санитизирует тело запроса, удаляя конфиденциальные данные.
+// Возвращает карту с данными.
+func sanitizeRequestBody(r *http.Request, mask string, filerKeys []string) map[string]any {
+	defer r.Body.Close()
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil
+	}
+
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	var body map[string]any
+	if err := json.Unmarshal(bodyBytes, &body); err != nil {
+		return nil
+	}
+
+	for _, key := range filerKeys {
+		if _, ok := body[key]; ok {
+			body[key] = mask
+		}
+	}
+
+	return body
 }
